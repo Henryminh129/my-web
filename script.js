8c187b0ca9b9792dab075d9ae8544fccd894149f
Clb Sáng tạo trẻ <156094841+Henryminh129@users.noreply.github.com>
2024-04-27 17:59:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,84 @@
-var typed = new Typed(".typing",{
-    strings:["Web designer","Web Developer","Video editor","Student"],
+var typed = new Typed(".typing", {
+    strings: ["Artificial intelligent", "Web Develop", "Video editing"],
     typeSpeed: 100,
-    BackSpeed:60,
+    BackSpeed: 60,
     loop: true
 })
 // aside
 const nav = document.querySelector(".nav"),
-        navList = nav.querySelectorAll("li"),
-        totalNavList = navList.length,
-        allSection = document.querySelectorAll(".section"),
-        totalSection = allSection.length
-        for(let i = 0; i<totalNavList; i++)
-        {
-            const a = navList[i].querySelector("a")
-            a.addEventListener("click", function()
-            {
-                for(let i = 0; i<totalSection;i++)
-                {
-                allSection[i].classList.remove("back-section");
-                }
-                for(let j = 0; j<totalNavList; j++)
-                {
-                    if(navList[j].querySelector("a").classList.contains("active"))
-                    {
-                        allSection[j].classList.add("back-section");
-                    }
-                    navList[j].querySelector("a").classList.remove("active");
-                }
-                this.classList.add("active")
-                showSection(this);
-                if(window.innerWidth <1200)
-                {
-                    asideSectionTogglerBtn();
-                }
-            })
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length
+
+for (let i = 0; i < totalNavList; i++) {
+
+    const a = navList[i].querySelector("a")
+    a.addEventListener("click", function () {
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove("back-section");
         }
-        function showSection(element)
-        {
-            for(let i = 0; i<totalSection;i ++)
-            {
-                allSection[i].classList.remove("active");
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                allSection[j].classList.add("back-section");
             }
-            const target = element.getAttribute("href").split("#")[1];
-            document.querySelector("#" + target).classList.add("active")
+            navList[j].querySelector("a").classList.remove("active");
         }
-        const navTogglerBtn = document.querySelector(".nav-toggler"),
-            aside = document.querySelector(".aside");
-            navTogglerBtn.addEventListener("click", () =>
-            {
-                asideSectionTogglerBtn();
-            })
-            function asideSectionTogglerBtn()
-            {
-                aside.classList.toggle("open");
-                navTogglerBtn.classList.toggle("open");
-                for(let i = 0; i<totalSection; i++)
-                {
-                    allSection[i].classList.toggle("open");
-                }
-            }
-        function removeBackSection(){
-            for(let i = 0; i < totalSection; i++)
-            {
-                allSection[i].classList.remove("back-section");
-            }
+        this.classList.add("active")
+        showSection(this);
+        if (window.innerWidth < 1200) {
+            asideSectionTogglerBtn();
         }
-        function addBackSection(num){
-            allSection[num].classList.add("back-section")
+    })
+}
+
+const showSection = (element) => {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+const asideSectionTogglerBtn = () => {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
+}
+
+
+const removeBackSection = () => {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+
+const addBackSection = (num) => {
+    allSection[num].classList.add("back-section")
+}
+
+const updateNav = (element) => {
+    for (i = 0; i < totalNavList; i++) {
+        navList[i].querySelector("a").classList.remove("active")
+        const target = element.getAttribute("href").split("#")[1];
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
+            navList[i].querySelector("a").classList.add("active");
         }
-        function updateNav(element)
-        {
-            for(i = 0; i<totalNavList;i++)
-            {
-                navList[i].querySelector("a").classList.remove("active")
-                const target = element.getAttribute("href").split("#")[1];
-                if(target===navList[i].querySelector("a").getAttribute("href").split("#")[1])
-                {
-                    navList[i].querySelector("a").classList.add("active");
-                }
-            }
-        }
-            document.querySelector(".hire-me").addEventListener("click", function()
-            {
-                const sectionIndex = this.getAttribute("data-section-index");
-                showSection(this);
-                updateNav(this)
-                removeBackSection();
-                addBackSection(sectionIndex);
-            })
-        
+    }
+}
+document.querySelector(".hire-me").addEventListener("click", function () {
+    const sectionIndex = this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this)
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
+
